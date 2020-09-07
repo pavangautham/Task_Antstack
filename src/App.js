6fea3ca95a00c79856ac03c9e6fca81368670171
pavan gautham <pavan.gautham17@gmail.com>
2020-09-07 23:08:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,88 +4,73 @@ import CsvReader from './CsvReader';
 import { Table } from 'antd'
 
 function App() {
-  const [csvData, setCsvData] = useState([]);
-  const [searched, setSearched] = useState([]);
-  const [pinCode, setPinCode] = useState("");
-  const [orderDate, setOrderDate] = useState("");
+    const [csvData, setCsvData] = useState([]);
+    const [searched, setSearched] = useState([]);
+    const [pinCode, setPinCode] = useState("");
+    const [orderDate, setOrderDate] = useState("");
   
-  const columns = [
+    const columns = [
     {
-      title: 'Order Id',
-      dataIndex: 'orderId',
-      key: 'orderId',
-      sorter: (a, b) => a.orderId - b.orderId,
+        title: 'Order Id',
+        dataIndex: 'orderId',
+        key: 'orderId',
+        sorter: (a, b) => a.orderId - b.orderId,
     },
     {
-      title: 'Customer Id',
-      dataIndex: 'customerId',
-      key: 'customerId'
+        title: 'Customer Id',
+        dataIndex: 'customerId',
+        key: 'customerId'
     },
     {
-      title: 'Pin Code',
-      dataIndex: 'deliveryPincode',
-      key: 'deliveryPincode'
+        title: 'Pin Code',
+        dataIndex: 'deliveryPincode',
+        key: 'deliveryPincode'
     },
     {
-      title: 'Order Date',
-      dataIndex: 'orderDate',
-      key: 'orderDate'
+        title: 'Order Date',
+        dataIndex: 'orderDate',
+        key: 'orderDate'
     },
     {
-      title: 'Items',
-      dataIndex: 'items',
-      key: 'items'
+        title: 'Items',
+        dataIndex: 'items',
+        key: 'items'
     },
   ];
 
-const filterItemsPin = (e) => {
-  const pin = new RegExp(`^${e.target.value}`, "i");
-  const date = new RegExp(`^${orderDate}`, "i");
-  const value = csvData.filter(list => pin.test(list.deliveryPincode) && date.test(list.orderDate))
-  setSearched(value);
-  setPinCode(e.target.value)
+//Filter PinCode
+const filterItemsPinCode = (text) => {
+    const pin = new RegExp(`^${text.target.value}`, "i");
+    const date = new RegExp(`^${orderDate}`, "i");
+    const value = csvData.filter(list => pin.test(list.deliveryPincode) && date.test(list.orderDate))
+    setSearched(value);
+    setPinCode(text.target.value)
 }
 
-const filterItemsDate = (e) => {
-  const pin = new RegExp(`^${pinCode}`, "i");
-  const date = new RegExp(`^${e.target.value}`, "i");
-  const value = csvData.filter(list => pin.test(list.deliveryPincode) && date.test(list.orderDate))
-  setSearched(value);
-  setOrderDate(e.target.value)
+//Filter OrderDate
+const filterItemsOrderDate = (text) => {
+    const pin = new RegExp(`^${pinCode}`, "i");
+    const date = new RegExp(`^${text.target.value}`, "i");
+    const value = csvData.filter(list => pin.test(list.deliveryPincode) && date.test(list.orderDate))
+    setSearched(value);
+    setOrderDate(text.target.value)
 }
 
-  console.log(csvData)
   return (
-
 <>
     <div className="App">
-      <CsvReader 
-        getResult={res => {
-          setCsvData(res.data);
-          setSearched(res.data)
-        }}
-      />
-      <div>          
-        <label className="inputfield">
-        Pincode:        
-          <input 
-          value={pinCode}
-          onChange={e => {
-            filterItemsPin(e)
-            }}/>
-
-            
-        </label>
-        <label className="inputfield">
-        Date:        
-          <input 
-          value={orderDate}
-          onChange={e => {
-            filterItemsDate(e)
-            }}/>
-        </label>        
-      </div>
-      <Table columns={columns} dataSource={searched} />
+        <CsvReader 
+          getResult={res => { setCsvData(res.data); setSearched(res.data) }}
+        />
+        <div>          
+            <label className="inputfield"> Pincode:        
+                <input placeholder="Enter PinCode" value={pinCode} onChange={text => { filterItemsPinCode(text) }}/>
+            </label>
+            <label className="inputfield"> Date:        
+                <input placeholder="Enter date (dd/mm/yyyy)" value={orderDate} onChange={text => { filterItemsOrderDate(text) }}/>
+            </label>        
+        </div>
+        <Table columns={columns} dataSource={searched} />
     </div>
 </>
   );
